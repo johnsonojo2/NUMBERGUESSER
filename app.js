42ef5d954ec61793babf51278fb9470a40948a4f
Johnson ojo <johnsonojo2@gmail.com>
2022-09-24 08:55:40 -0600

    adding functionalities

--- app.js
+++ app.js
@@ -4,3 +4,29 @@ let min = 1,
   max = 10,
   winningNum = 2,
   guessesLeft = 3;
+
+//UI Elements
+
+const game = document.querySelector("#game"),
+  minNum = document.querySelector(".min-num"),
+  maxNum = document.querySelector(".max-num"),
+  guessInput = document.querySelector("#guess-input"),
+  guessBtn = document.querySelector("#guess-btn"),
+  message = document.querySelector(".message");
+
+minNum.textContent = min;
+maxNum.textContent = max;
+
+guessBtn.addEventListener("click", function () {
+  let guess = parseInt(guessInput.value);
+  if (isNaN(guess) || guess < min || guess > max) {
+    setMessage(`Please enter a number between ${min} and ${max}`, "red");
+  }
+  //   console.log(typeof parseInt(guessInput.value));
+});
+function setMessage(msg, color) {
+  //   message.style.display = "block";
+  //   message.style.transition = "display 10s";
+  message.textContent = msg;
+  message.style.color = color;
+}
